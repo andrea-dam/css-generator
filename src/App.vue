3710dd2ff1e73d0b54a2da3afacd93d07f9a0345
Andrea Damiani <andrea.dam90@gmail.com>
2023-06-18 21:25:37 +0200

    Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+    <aside class="col-span-2 bg-slate-400">
+        <ul>
+            <li>
+                <button @click="showComponent(BoxShadow)">Box Shadow</button>
+            </li>
+        </ul>
+    </aside>
+
+    <main class="col-span-10 flex items-center justify-center bg-neutral-50 px-24 py-12">
+        <div class="grid h-full w-full grid-rows-6 rounded border bg-white shadow-inner">
+            <Component :is="shownComponent"></Component>
+        </div>
+    </main>
+</template>
+
+<script setup>
+import { ref } from "vue";
+import BoxShadow from "/src/components/BoxShadow.vue";
+
+let shownComponent = BoxShadow;
+
+const showComponent = component => {
+    showComponent = component;
+};
+</script>
---
+++ src/components/BoxShadow.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="row-span-3 flex items-center justify-center">
+        <div class="box-shadow h-24 w-24 border bg-neutral-500"></div>
+    </div>
+    <div class="row-span-3">
+        <input type="range" name="h-offset" id="h-offset" v-model="hOffset" min="0" max="200" />
+        <input type="range" name="v-offset" id="v-offset" v-model="vOffset" />
+    </div>
+</template>
+
+<script setup>
+import { ref, watch } from "vue";
+import { useStyleTag } from "@vueuse/core";
+
+const hOffset = ref(0);
+const vOffset = ref(0);
+
+useStyleTag(`.box-shadow { box-shadow: ${hOffset.value}px ${vOffset.value}px; }`);
+</script>
